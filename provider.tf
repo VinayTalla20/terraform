6079507dd064ba4ad8fdd7660c6975146e04172f
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-09-09 12:33:11 +0530

    provider file for K8s

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+    config_path = "D:\terrafrom-k8s\configfile-k8s"
+
+}
